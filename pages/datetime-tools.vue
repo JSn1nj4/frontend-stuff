adca1a2607adeb3f8851be276d923690b2a938c0
Elliot Derhay <elliot@elliotderhay.com>
2025-02-22 14:51:56 -0500

    Input type selector works!

--- pages/datetime-tools.vue
+++ pages/datetime-tools.vue
@@ -7,28 +7,50 @@
     <TextHeading type="h2" class="text-slate-900 dark:text-slate-50 mb-3 font-normal">
       Tools for working with date/time strings and timestamps
     </TextHeading>
-     <div class="block sm:grid sm:grid-cols-2 gap-10 text-slate-800 dark:text-white mt-4 pt-4 border-t-2 border-t-emerald-500 border-dashed">
-       <div class="col-span-2">
-         <TextHeading type="h3" class="text-slate-900 dark:text-slate-50">
-           Format Conversion
-         </TextHeading>
-       </div>
-       <div>
-         <FormInputGroup
-           id='date-formatter-input'
-           name='date-formatter-input'
-           v-model="dateFormatter.input.value"
-         >
-           Input
-         </FormInputGroup>
-       </div>
-       <div>
-         <FormInputGroup
-           disabled
-           id="date-formatter-output"
-           name='date-formatter-output'
-           v-model="dateFormatter.output.value"
-         >
+    <div
+      class="block sm:grid sm:grid-cols-2 gap-10 text-slate-800 dark:text-white mt-4 pt-4 border-t-2 border-t-emerald-500 border-dashed"
+    >
+      <div class="col-span-2">
+        <TextHeading type="h3" class="text-slate-900 dark:text-slate-50">
+          Format Conversion
+        </TextHeading>
+      </div>
+      <div>
+        <Label for="date-input-type">Date Selector Type</Label>
+        <Select
+          v-model="dateInputType"
+          id="date-input-type"
+          name="date-input-type"
+        >
+          <SelectTrigger>
+            <SelectValue
+              placeholder="How do you want to select a date and time?"
+            />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem
+              v-for="(inputType, index) in dateInputTypes"
+              :key="`input-type-${index}`"
+              :value="inputType.value"
+              >{{ inputType.label }}
+            </SelectItem>
+          </SelectContent>
+        </Select>
+        <FormInputGroup
+          id="date-formatter-input"
+          name="date-formatter-input"
+          v-model="dateFormatter.input.value"
+        >
+          Input
+        </FormInputGroup>
+      </div>
+      <div>
+        <FormInputGroup
+          disabled
+          id="date-formatter-output"
+          name="date-formatter-output"
+          v-model="dateFormatter.output.value"
+        >
           Output
          </FormInputGroup>
        </div>
@@ -39,9 +61,16 @@
 <script setup lang="ts">
 import { globals } from '~/library/stores/globals'
 import { FieldIO, type IFieldIO } from '~/library/forms/FieldIO.class'
-import {getUnixTime} from 'date-fns/getUnixTime'
-import {pipe} from '~/library/helpers/pipes'
-import {formatISO, fromUnixTime} from 'date-fns'
+import { getUnixTime } from 'date-fns/getUnixTime'
+import { pipe } from '~/library/helpers/pipes'
+import { formatISO, fromUnixTime } from 'date-fns'
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select'
+import { Label } from '~/components/ui/label'
 
 const title = ref('Date/Time Tools')
 globals.pageTitle = title.value
@@ -79,10 +108,14 @@ const dateFormatter: IFieldIO<string> = new FieldIO({
   },
 })
 
-const dateInput = [
-  { label: 'Raw', value: 'default', selected: true },
+const dateInputTypes = [
+  { label: 'Raw', value: 'default' },
   { label: 'Date Picker', value: 'datepicker' },
-]
+  { label: 'Dropdowns', value: 'dropdowns' },
+] as const
+type dateInputTypeOptions = typeof dateInputTypes[number]['value']
+
+const dateInputType = ref<dateInputTypeOptions>('datepicker')
 
 const dateOutputFormats = [
   { label: 'Input', value: 'default', selected: true },
